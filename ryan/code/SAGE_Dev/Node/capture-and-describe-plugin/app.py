d05ad1a33bd75f3e6014311d0398edf05f2f1845
RyanRearden <reardenrt@gmail.com>
2024-07-29 10:42:12 -0500

    updated code to reflect W0B3 code

--- ryan/code/SAGE_Dev/Node/capture-and-describe-plugin/app.py
+++ ryan/code/SAGE_Dev/Node/capture-and-describe-plugin/app.py
@@ -9,33 +9,8 @@ import argparse
 from transformers import AutoProcessor, AutoModelForCausalLM
 import requests
 import json
-import resource
 from PIL import Image
 
-
-
-def limit_memory():
-    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
-    resource.setrlimit(resource.RLIMIT_AS, (5 * 1024 * 1024 * 1024, soft))
-
-limit_memory()
-
-# Set CPU affinity to the first 4 cores
-def set_cpu_affinity(cores):
-    pid = os.getpid()
-    os.sched_setaffinity(pid, cores)
-
-set_cpu_affinity({0, 1})
-
-
-
-url = ''
-def sendMessage(text):
-   
-    headers = {'Content-type': 'application/json'}
-    data = {'text': text}
-
-    response = requests.post(url, headers=headers, data=json.dumps(data))
 
 def readImage(imgIpt):
     #opens image if its already on the computer
@@ -54,64 +29,60 @@ logging.basicConfig(
 model = AutoModelForCausalLM.from_pretrained("./Florence-2-base", local_files_only=True, trust_remote_code=True)
 processor = AutoProcessor.from_pretrained("./Florence-2-base", local_files_only=True, trust_remote_code=True)
 
-sendMessage("Just got started with the script on the node")
+
 #takes in a task prompt and image, returns an answer 
 def run_example(task_prompt, image, text_input=None):
-    sendMessage("run_example 1/5")
     if text_input is None:
         prompt = task_prompt
     else:
         prompt = task_prompt + text_input
     inputs = processor(text=prompt, images=image, return_tensors="pt")
-    try: 
-        sendMessage("run_example 2/5")
-        generated_ids = model.generate(
-        input_ids=inputs["input_ids"],
-        pixel_values=inputs["pixel_values"],
-        max_new_tokens=1024,
-        early_stopping=False,
-        do_sample=False,
-        num_beams=3,
-        )
-    except Exception as e:
-        sendMessage(e)
-    sendMessage("run_example 3/5")
+
+    generated_ids = model.generate(
+    input_ids=inputs["input_ids"],
+    pixel_values=inputs["pixel_values"],
+    #Changed from 1024 to 512
+    max_new_tokens=512,
+    #Changed from False to True
+    early_stopping=False,
+    do_sample=False,
+    #changed from 3 to 2
+    num_beams=2,
+    )
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=False)[0]
-    sendMessage("run_example 4/5")
+
     parsed_answer = processor.post_process_generation(
         generated_text, 
         task=task_prompt, 
         image_size=(image.width, image.height)
     )
-    sendMessage("run_example 5/5")
+
     return parsed_answer
 
 #takes in an image (img), returns a description (string)
 def generateDescription(image):
     task_prompt = '<MORE_DETAILED_CAPTION>'
-    sendMessage("set task to MDC")
+
     description_text = run_example(task_prompt, image)
     description_text = description_text[task_prompt]
 
     #takes those details from the setences and finds labels and boxes in the image
     task_prompt = '<CAPTION_TO_PHRASE_GROUNDING>'
     boxed_descriptions = run_example(task_prompt, image, description_text)
-    sendMessage("set task to CTPG")
 
     #only prints out labels not bboxes
     descriptions = boxed_descriptions[task_prompt]['labels']
+    logging.info(descriptions)
 
 
     #finds other things in the image that the description did not explicitly say
     task_prompt = '<DENSE_REGION_CAPTION>'
-    sendMessage("set task to DRC")
     labels = run_example(task_prompt, image)
 
     #only prints out labels not bboxes
     printed_labels = labels[task_prompt]['labels']
     
     description = "".join([item for sublist in [description_text, descriptions, printed_labels] for item in sublist])
-    sendMessage("Made a description!")
     return description
 
 
@@ -120,36 +91,32 @@ def capture(plugin, cam, args):
     text_file_name = "description.txt"
     sample = cam.snapshot()
     print(sample)
-    sendMessage("Took a picture")
     if args.out_dir == "":
         sample.save(sample_file_name)
         img = readImage(sample_file_name)
         description = generateDescription(img)
-        sendMessage("Finished making my description")
         with open(text_file_name, "w") as text_file:
             text_file.write(description)
-        plugin.upload_file(sample_file_name)
+        plugin.upload_file(sample_file_name, meta={"description": f"1"})
         plugin.upload_file(text_file_name)
+
     else:
         dt = datetime.fromtimestamp(sample.timestamp / 1e9)
         base_dir = os.path.join(args.out_dir, dt.astimezone(timezone.utc).strftime('%Y/%m/%d/%H'))
         os.makedirs(base_dir, exist_ok=True)
-        sample_path = os.path.join(base_dir, dt.astimezone(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S%z.jpg'))
-        text_path = os.path.join(base_dir, "description.txt")
+        timeName = dt.astimezone(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S%z')
+        sample_path = os.path.join(base_dir, f'{timeName}.jpg')
+        text_path = os.path.join(base_dir, f'{timeName}.txt')
         sample.save(sample_path)
-        img = readImage(sample_file_name)
+        img = readImage(sample_path)
         description = generateDescription(img)
-        sendMessage("Finished making my description")
         with open(text_path, "w") as text_file:
             text_file.write(description)
-        sendMessage("Uploading")
-        sendMessage(description)
-        plugin.upload_file(sample_path)
+        plugin.upload_file(sample_path, meta={"description": f"{2}"})
         plugin.upload_file(text_path)
 
 def run(args):
     logging.info("starting image sampler.")
-    sendMessage("starting up in the run function")
     if args.cronjob == "":
         logging.info("capturing...")
         with Plugin() as plugin, Camera(args.stream) as cam:
@@ -173,12 +140,11 @@ def run(args):
             logging.info("capturing...")
             with Camera(args.stream) as cam:
                 capture(plugin, cam, args)
-                sendMessage("Done with the run function")
     return 0
 
 
 if __name__ == '__main__':
-    sendMessage("In main")
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-stream', dest='stream',
